898088dd604daa33addfc8ba1343ecfc94031d46
aditya <aditya.nigudkar@gmail.com>
2022-03-31 19:57:55 +0530

    Pascal triangle

--- src/main/java/BackTracking/A9.java
+++ src/main/java/BackTracking/A9.java
@@ -1,9 +1,13 @@
 package BackTracking;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class A9 {
     public static void main(String[] args) {
-        char[] str = new char[]{'A','B','C'};
-        permute(str,0);
+        //char[] str = new char[]{'A','B','A','A'};
+        int[] intArr = {1,1,2};
+        permuteUnique(intArr);
     }
     /**
      * Permutation of a String
@@ -34,4 +38,47 @@ public class A9 {
          str[i] = str[j];
          str[j] = t;
      }
+
+
+     private static void swap(int[] intArr, int i, int j){
+        int t = intArr[i];
+         intArr[i] = intArr[j];
+         intArr[j] = t;
+     }
+
+     /**
+      * https://leetcode.com/problems/permutations-ii/
+      *
+      * */
+      public static List<List<Integer>>  permuteUnique(int[] nums) {
+          List<List<Integer>> resultList = new ArrayList<>();
+          permuteUniqueInternal(nums,0,resultList);
+          return resultList;
+      }
+
+      private static void permuteUniqueInternal(int[] nums, int i,List<List<Integer>> result) {
+          if(i== nums.length){
+              List<Integer> numList = new ArrayList<>();
+              for (int k:nums
+                   ) {
+                  numList.add(k);
+              }
+              result.add(numList);
+              return;
+          }
+          Map<Integer, Integer> freqMap = new HashMap<>();
+          for(int j=i;j< nums.length;j++){
+              if(freqMap.get(nums[j])==null ){
+                  swap(nums,i,j);
+                  permuteUniqueInternal(nums,i+1,result);
+                  swap(nums,i,j);
+              }
+              if(freqMap.get(nums[j])!=null){
+                  freqMap.put(nums[j], freqMap.get(nums[j])+1);
+              }
+              else{
+                  freqMap.put(nums[j],1);
+              }
+          }
+      }
 }
