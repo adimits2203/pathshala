417272057f81fbbef86799a03acd31a7ad29af03
aditya <aditya.nigudkar@gmail.com>
2022-04-15 22:27:54 +0530

    Pascal triangle

--- src/main/java/searching/S1.java
+++ src/main/java/searching/S1.java
@@ -1,5 +1,6 @@
 package searching;
 
+import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 public class S1 {
@@ -14,11 +15,111 @@ public class S1 {
         for (int i = 0; i <  size; i++) {
             arr[i] = Integer.parseInt(chars[i]);
         }
-        int tc = sc.nextInt();
-        while (tc-- > 0){
-            int input = sc.nextInt();
-            System.out.println(binarySearchAndSuggestIndex(arr,input ));
-        }
+        findFirstOne(arr,1);
+     }
+
+
+     /**
+      *
+      * First 1 in Array
+      * Given a sorted array consisting of 0s and 1s
+      * . The task is to print the index of first 1
+      *  in the given array. You must write an algorithm with O(log(n))runtime complexity.
+      *
+      *  Sample Input 0
+      *
+      * 5
+      * 0 0 0 1 1
+      *
+      * Sample Output 0
+      *
+      * 4
+      * */
+      private static void findFirstOne(int[] arr, int key){
+          int low = 0, high = arr.length - 1;
+          while(low <= high){
+              int mid = (low + high) / 2;
+              if(arr[mid] == key){
+                  if(arr[mid-1] != key || mid == 0 ){
+                      System.out.println(++mid+ " ");
+                      return;
+                  }
+                  else{
+                      high = mid - 1;
+                  }
+              }
+              else if(arr[mid]<key){
+                  low = mid + 1;
+              }
+              else{
+                  high = mid - 1;
+              }
+          }
+          System.out.println(-1);
+      }
+
+     /**
+      *
+      * First and Last Position
+      * Given an array of integers
+      * nums
+      *  sorted in ascending order, find the starting and ending position of a given
+      * target  value.
+      *
+      * Sample Input
+      *
+      * 5
+      * 1 3 3 4 5
+      * 2
+      * 3
+      * 2
+      *
+      * Sample Output
+      *
+      * 2 3
+      * -1
+      *
+      * */
+     private static void findFirstAndLastIndex(int[] arr, int key){
+         int low = 0, high = arr.length - 1;
+         while(low <= high){
+             int mid = (low + high) / 2;
+             if(arr[mid] == key){
+                 if(arr[mid-1] != key || mid == 0 ){
+                     System.out.println(++mid+ " ");
+                     break;
+                 }
+                 else{
+                     high = mid - 1;
+                 }
+             }
+             else if(arr[mid]<key){
+                 low = mid + 1;
+             }
+             else{
+                 high = mid - 1;
+             }
+         }
+         low = 0; high = arr.length-1;
+         while(low <= high){
+             int mid = (low + high) / 2;
+             if(arr[mid] == key){
+                 if(arr[mid+1] != key || mid == arr.length - 1 ){
+                     System.out.print(++mid);
+                     return;
+                 }
+                 else{
+                     low = mid + 1;
+                 }
+             }
+             else if(arr[mid]<key){
+                 low = mid + 1;
+             }
+             else{
+                 high = mid - 1;
+             }
+         }
+         System.out.println((-1) );
      }
 
     /**
