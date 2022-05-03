50b0728fbd2972fd2d817c9f145fa79940d95d26
aditya nigudkar <adimits@gmail.com>
2022-05-03 11:25:50 +0530

    revision

--- src/main/java/twoPointers/tpMain.java
+++ src/main/java/twoPointers/tpMain.java
@@ -1,10 +1,45 @@
 package twoPointers;
 
+import java.util.Arrays;
+
 public class tpMain {
 
     public static void main(String[] args) {
-        lengthOfLongestSubstring("abcabcbb");
+        System.out.println(hasArrayTwoCandidates(new int[]{1, 4, 45, 6, 10, 8}, 16) ? "Yes" : "No");
+
     }
+
+    /**
+     * https://practice.geeksforgeeks.org/problems/key-pair5616/1
+     * Given an array Arr of N positive integers and another number X. Determine whether or not there exist two elements in Arr whose sum is exactly X.
+     *
+     * Example 1:
+     *
+     * Input:
+     * N = 6, X = 16
+     * Arr[] = {1, 4, 45, 6, 10, 8}
+     * Output: Yes
+     * Explanation: Arr[3] + Arr[4] = 6 + 10 = 16
+     * */
+     private static boolean hasArrayTwoCandidates(int[] arr, int x){
+         Arrays.sort(arr);
+         int l=0, r=arr.length -1;
+
+         while(l < r){
+             int n = x - arr[l];
+             if(arr[r]==n){
+                 return true;
+             }
+             else if(arr[r]<n){
+                 l++;
+             }
+             else{
+                 r--;
+             }
+         }
+         return false;
+
+     }
 
     /**
      * https://leetcode.com/problems/longest-substring-without-repeating-characters/
