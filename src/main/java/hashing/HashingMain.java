112e92ce36b47c533ff464323587cb8ed0a95c2a
aditya <aditya.nigudkar@gmail.com>
2022-06-27 20:26:36 +0530

    Pascal triangle

--- src/main/java/hashing/HashingMain.java
+++ src/main/java/hashing/HashingMain.java
@@ -6,8 +6,11 @@ import java.util.Map;
 public class HashingMain {
 
     public static void main(String[] args) {
+        String s = "0|1|2|3|4";
+        String[] values        =    s.split("\\|");
+        System.out.println(values[2]);
 
-        System.out.println(lenOfLongSubarr(new int[]{-13 ,0, 6, 15, 16, 2, 15, -12, 17, -16, 0, -3, 19, -3, 2, -9, -6}, 17,15));
+        //System.out.println(lenOfLongSubarr(new int[]{-13 ,0, 6, 15, 16, 2, 15, -12, 17, -16, 0, -3, 19, -3, 2, -9, -6}, 17,15));
     }
 
 
---
+++ src/main/java/twoPointers/TwoPointersMain.java
@@ -0,0 +1,168 @@
+package twoPointers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TwoPointersMain
+{
+
+    public static void main(String[] args) {
+        System.out.println(threeSum(new int[]{-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6}));
+    }
+
+    /**
+     * https://practice.geeksforgeeks.org/problems/key-pair5616/1
+     * Given an array Arr of N positive integers and another number X. Determine whether or not there exist two elements in Arr whose sum is exactly X.
+     * Example 1:
+     *
+     * Input:
+     * N = 6, X = 16
+     * Arr[] = {1, 4, 45, 6, 10, 8}
+     * Output: Yes
+     * Explanation: Arr[3] + Arr[4] = 6 + 10 = 16
+     *
+     *
+     * Example 2:
+     *
+     * Input:
+     * N = 5, X = 10
+     * Arr[] = {1, 2, 4, 3, 6}
+     * Output: Yes
+     * Explanation: Arr[2] + Arr[4] = 4 + 6 = 10
+     *
+     *
+     * */
+     private static boolean hasArrayTwoCandidates(int[] arr, int sum){
+         int l = 0, r = arr.length-1;
+         Arrays.sort(arr);
+         while(l<r){
+             int target = sum - arr[l];
+             if(arr[r] == target){
+                 System.out.println(arr[l]+ " + "+ arr[r] + " = "+ sum);
+                 return true;
+             }
+             else if(arr[r] > target){
+                 r--;
+             }
+             else {
+                 l++;
+             }
+         }
+
+         return false;
+     }
+
+     /**
+      * Count pairs with given sum[https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/1]
+      * Given an array of N integers, and an integer K, find the number of pairs of elements in the array whose sum is equal to K.
+      *
+      * Example 1:
+      *
+      * Input:
+      * N = 4, K = 6
+      * arr[] = {1, 5, 7, 1}
+      * Output: 2
+      * Explanation:
+      * arr[0] + arr[1] = 1 + 5 = 6
+      * and arr[1] + arr[3] = 5 + 1 = 6.
+      *
+      * Example 2:
+      *
+      * Input:
+      * N = 4, K = 2
+      * arr[] = {1, 1, 1, 1}
+      * Output: 6
+      * Explanation:
+      * Each 1 will produce sum 2 with any 1.
+      * */
+      private static int getPairsCount(int[] arr, int sum){
+          int count = 0;
+          int l = 0, r = arr.length-1;
+          Arrays.sort(arr);
+          while(l<r){
+              int target = sum - arr[l];
+              if(arr[r] == target){
+                  if(arr[r] == arr[l]){
+                      int v1= arr[l], v2=arr[r], cnt1=0, cnt2=0;
+                      while(arr[l]==v1){
+                          l++;
+                          cnt1++;
+                      }
+                      while(arr[r]==v2){
+                          r--;
+                          cnt2++;
+                      }
+                    //  count +=
+                  }
+                  else{
+                      l++;
+                      r--;
+                  }
+              }
+              else if(arr[r] > target){
+                  r--;
+              }
+              else {
+                  l++;
+              }
+          }
+          return count;
+      }
+
+
+      /**
+       * https://leetcode.com/problems/3sum/
+       * triplet sum
+       * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+       *
+       * Notice that the solution set must not contain duplicate triplets.
+       * Example 1:
+       *
+       * Input: nums = [-1,0,1,2,-1,-4]
+       * Output: [[-1,-1,2],[-1,0,1]]
+       * Example 2:
+       *
+       * Input: nums = []
+       * Output: []
+       *
+       * Example 3:
+       *
+       * Input: nums = [0]
+       * Output: []
+       *[-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]
+       * Idea is for a+b+c=0, if a is fixes find b & c such that b+c = -a using two pointers pos sorting
+       *
+       * */
+      public static List<List<Integer>> threeSum(int[] nums) {
+          List<List<Integer>> result = new ArrayList<>();
+          Arrays.sort(nums);
+          for(int i=0;i< nums.length-1;i++){
+              if(i>0 && nums[i-1]==nums[i]) continue;
+              int a = nums[i];
+              int t = -1 * (a);
+              int p1= i+1, p2=nums.length-1;
+              while(p1<p2) {
+                  if (nums[p1] + nums[p2] < t) p1++;
+                  else if (nums[p1] + nums[p2] > t) p2--;
+                  else {// sum is equal
+                      List<Integer> list = new ArrayList<>();
+                      list.add(nums[p1]);
+                      list.add(nums[p2]);
+                      list.add(a);
+                      result.add(list);
+                      if (nums[p1] == nums[p2]) {
+                          break;
+                      }
+                      int x = nums[p1], y = nums[p2];
+                      while (nums[p1] == x) p1++ ;
+                      while (nums[p2] == y) p2--;
+                  }
+              }
+          }
+          return result;
+
+      }
+
+
+}
