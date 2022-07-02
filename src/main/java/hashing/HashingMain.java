562b101b6d9ca8b666e4ca64cb2915d76d94bf80
aditya nigudkar <adimits@gmail.com>
2022-07-02 23:42:32 +0530

    revision

--- src/main/java/hashing/HashingMain.java
+++ src/main/java/hashing/HashingMain.java
@@ -1,14 +1,14 @@
 package hashing;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.ClientInfoStatus;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class HashingMain {
 
     public static void main(String[] args) {
         String s = "0|1|2|3|4";
-        String[] values        =    s.split("\\|");
-        System.out.println(values[2]);
+        System.out.println(reverse(s));
 
         //System.out.println(lenOfLongSubarr(new int[]{-13 ,0, 6, 15, 16, 2, 15, -12, 17, -16, 0, -3, 19, -3, 2, -9, -6}, 17,15));
     }
@@ -135,7 +135,7 @@ public class HashingMain {
                   System.out.println("got the sum: "+(sum-K) +" at index : "+ map.get(sum-K));
                   result = Math.max(result, i-map.get(sum-K));
                   System.out.println("result is updated to : "+ result);
-              } if(map.get(sum)==null){
+              }if(map.get(sum)==null){
                   map.put(sum, i);
               }
           }
@@ -145,6 +145,123 @@ public class HashingMain {
           return  result;
       }
 
+
+      /**
+       * https://leetcode.com/problems/palindrome-pairs/
+       *
+       * Given a list of unique words, return all the pairs of the distinct indices (i, j) in the given list, so that the concatenation of the two words words[i] + words[j] is a palindrome.
+       *
+       *
+       *
+       * Example 1:
+       *
+       * Input: words = ["abcd","dcba","lls","s","sssll"]
+       * Output: [[0,1],[1,0],[3,2],[2,4]]
+       * Explanation: The palindromes are ["dcbaabcd","abcddcba","slls","llssssll"]
+       * Example 2:
+       *
+       * Input: words = ["bat","tab","cat"]
+       * Output: [[0,1],[1,0]]
+       * Explanation: The palindromes are ["battab","tabbat"]
+       * Example 3:
+       *
+       * Input: words = ["a",""]
+       * Output: [[0,1],[1,0]]
+       *
+       * Idea is to :
+       *  1. put reverse of each word with index in a map m
+       *  2. iterate over the list of words and for each word:
+       *    2.1. iterate over all the prefixes and for each prefix:
+       *        2.1.1. check for empty prefix/suffix all the conditions of 2.1.2.
+       *        2.1.2. check if the prefix exists in the m AND if the index is not same as current words index(ie not a self pointer)
+       *        AND remaining suffix of this word is palindrome AND this combination is not repeated then
+       *        add this i,j into another map(to check duplication) and add this to result list
+       *        2.1.3. repeat 2.1.2 for suffix
+       *
+       *
+       * */
+      public List<List<Integer>> palindromePairs(String[] words) {
+          List<List<Integer>> result = new ArrayList<>();
+          Map<String, Integer> reverseWordMap = new HashMap<>();
+          int i =0;
+          for (String word: words
+               ) {
+              reverseWordMap.put(reverse(word),i++);
+          }
+          Map<Pair, Boolean> duplicacyCheckMap = new HashMap();
+          for(int j=0;j<words.length;j++){// empty
+              if(reverseWordMap.get("")!=null && reverseWordMap.get("")!=j && isPalindrome(words[j])  && duplicacyCheckMap.get(new Pair(j,reverseWordMap.get("")))==null){
+                    duplicacyCheckMap.put(new Pair(j,reverseWordMap.get("")),true);
+                    duplicacyCheckMap.put(new Pair(reverseWordMap.get(""),j),true);
+                    List l1 = new ArrayList();
+                    l1.add(j);
+                    l1.add(reverseWordMap.get(""));
+                    List l2 = new ArrayList();
+                    l2.add(reverseWordMap.get(""));
+                    l2.add(j);
+                    result.add(l1);
+                    result.add(l2);
+              }
+          }
+
+          for(int j=0;j<words.length;j++) {// empty
+
+          }
+              return result;
+      }
+
+    private boolean isPalindrome(String word) {
+          char[] charArr = word.toCharArray();
+          int i=0, j=charArr.length-1;
+          while(i<j){
+              if(charArr[i]!=charArr[j]) return false;
+          }
+          return true;
+    }
+
+    class Pair{
+           int p;
+            int q;
+
+          public Pair(int a,int b){
+              p=a;
+              q=b;
+          }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            return p == pair.p && q == pair.q;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(p, q);
+        }
+
+        @Override
+        public String toString() {
+            return "Pair{" +
+                    "p=" + p +
+                    ", q=" + q +
+                    '}';
+        }
+    }
+
+    private static String reverse(String word) {
+          char[] charArr = word.toCharArray();
+          int i=0,j=charArr.length-1;
+          while(i<j){
+              char c = charArr[i];
+              charArr[i] = charArr[j];
+              charArr[j] = c;
+              i++;
+              j--;
+          }
+          return new String(charArr);
+    }
 
 
 }
