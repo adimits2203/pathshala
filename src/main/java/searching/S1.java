39f1d8bb3ea841ccf61f6ab0596437ffacf993ef
aditya <aditya.nigudkar@gmail.com>
2022-07-04 20:20:09 +0530

    Pascal triangle

--- src/main/java/searching/S1.java
+++ src/main/java/searching/S1.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class S1 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int[] arr = new int[size];
         sc.nextLine();
@@ -16,7 +16,9 @@ public class S1 {
             arr[i] = Integer.parseInt(chars[i]);
         }
         int k = sc.nextInt();
-        System.out.println(findKthSmallest(arr,k));
+        System.out.println(findKthSmallest(arr,k));*/
+
+        System.out.println(findPeakElement(new int[]{1,2,1,3,5,6,4}));
      }
 
 
@@ -379,6 +381,53 @@ public class S1 {
                if(a<=e) counter++;
            }
            return counter;
+       }
+
+
+       /** https://leetcode.com/problems/find-peak-element/ **
+        * A peak element is an element that is strictly greater than its neighbors.
+        *
+        * Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+        *
+        * You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+        *
+        * You must write an algorithm that runs in O(log n) time.
+        *
+        * Example 1:
+        *
+        * Input: nums = [1,2,3,1]
+        * Output: 2
+        * Explanation: 3 is a peak element and your function should return the index number 2.
+        * Example 2:
+        *
+        * Input: nums = [1,2,1,3,5,6,4]
+        * Output: 5
+        * Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+        *
+        *
+        * */
+       public static int findPeakElement(int[] nums) {
+           if(nums.length<2) return 0;
+           if(nums.length==2){
+               if(nums[0]>nums[1]) return 0;
+               else return 1;
+           }
+
+           int l=0,h=nums.length-1;
+
+           while(l<=h) {
+               int m = (l+h)/2;
+               if ((m==0 || nums[m] > nums[m - 1]) && (m==nums.length-1 || nums[m] > nums[m + 1])) {
+                   return m ;
+               } else {
+                   if (nums[m + 1] > nums[m]) {
+                       l = m + 1;
+                   } else {
+                       h = m - 1;
+                   }
+               }
+           }
+           return 0;
        }
 
 
