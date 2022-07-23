25217a72ea284ef58341d9e20bfdffcb1c4d65c6
aditya nigudkar <adimits@gmail.com>
2022-07-23 14:58:05 +0530

    revision

--- src/main/java/stack/MyStack.java
+++ src/main/java/stack/MyStack.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class MyStack {
 
     public static void main(String[] args) {
-        System.out.println(isValid("]["));
+        System.out.println(isValidN("babcc"));
     }
 
     /**
@@ -81,6 +81,70 @@ public class MyStack {
     private static boolean isOpen(char c) {
            return c=='(' || c=='{' || c=='[';
     }
+
+
+    /***/
+    private static String plugin(String s){
+        Stack<Character> stk = new Stack();
+        for (char c:s.toCharArray()
+             ) {
+            if(stk.isEmpty()){
+                stk.push(c);
+            }
+            else{
+                if(stk.peek()==c){
+                    stk.pop();
+                }
+                else{
+                    stk.push(c);
+                }
+            }
+
+        }
+
+        return (stk.toString());
+
+    }
+
+
+    /**
+     * https://leetcode.com/problems/check-if-word-is-valid-after-substitutions/
+     *
+     * Idea is to use Stack to push the characters in and pop as soon as abc is achieved
+     *
+     * */
+    public static boolean isValidN(String s) {
+        Stack<Character> stack = new Stack();
+        for (char c:s.toCharArray()
+             ) {
+            if(stack.isEmpty()){
+                stack.push(c);
+            }
+            else{
+                if(c=='c' && stack.size()>=2){
+                    char b = stack.pop();
+                    char a = stack.pop();
+                    if(b=='b' && a=='a'){
+                        continue;
+                    }
+                    else{
+                        stack.push(a);
+                        stack.push(b);
+                        stack.push(c);
+                    }
+                }
+                else{
+                    stack.push(c);
+                }
+            }
+        }
+        if(stack.isEmpty() || stack.toString()=="abc"){
+            return true;
+        }
+        return false;
+    }
+
+
 
 
 }
