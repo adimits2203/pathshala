fd8e5fc11fcc2aef6c843538c1cb79603cc703b3
aditya <aditya.nigudkar@gmail.com>
2022-08-30 22:49:08 +0530

    Pascal triangle

--- src/main/java/sorting/SortingMain.java
+++ src/main/java/sorting/SortingMain.java
@@ -1,12 +1,16 @@
 package sorting;
 
+import javafx.util.Pair;
+
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 public class SortingMain {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+      /*  Scanner sc = new Scanner(System.in);
         int tcs = sc.nextInt();
         List<Integer[]> list = new ArrayList<>();
         while(tcs-- > 0){
@@ -26,12 +30,68 @@ public class SortingMain {
         for (Integer[] ip: list
              ) {
          freqSort(ip);
+        }*/
+
+        int[][] res = kClosest(new int[][]{{3,3},{5,-1},{-2,4}},2);
+
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(res[i][j]+",");
+            }
+            System.out.println();
         }
 
 
 
 
 
+    }
+
+
+
+    /**
+     * https://leetcode.com/problems/k-closest-points-to-origin/
+     *
+     * Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+     *
+     * The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+     *
+     * You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+     *
+     *Input: points = [[1,3],[-2,2]], k = 1
+     * Output: [[-2,2]]
+     * Explanation:
+     * The distance between (1, 3) and the origin is sqrt(10).
+     * The distance between (-2, 2) and the origin is sqrt(8).
+     * Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+     * We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
+     *
+     * Idea is to use sorting to sort the 'points' by the distance d = x2+y2
+     * */
+    public static int[][] kClosest(int[][] points, int k) {
+        List<Pair<Pair<Integer, Integer>, Integer>> pairList = new ArrayList<>();
+        for(int i =0;i< points.length;i++){
+            int d = points[i][0]*points[i][0] + points[i][1]*points[i][1];
+            pairList.add(new Pair(new Pair(points[i][0],points[i][1]), d));
+        }
+        Collections.sort(pairList, (o1, o2) -> {
+            if(o1.getValue() < o2.getValue()){
+                return -1;
+            }
+            else if(o1.getValue() > o2.getValue()){
+                return 1;
+            }
+            return 0;
+        });
+        int[][] res = new int[k][2];
+        int i =0;
+        while (k-- > 0){
+            res[i][0] = pairList.get(i).getKey().getKey();
+            res[i][1] = pairList.get(i).getKey().getValue();
+            i++;
+        }
+
+        return res;
     }
 
     /**
