042d689d51d5df98187871ab08ad9550797785c8
aditya nigudkar <adimits@gmail.com>
2022-09-18 18:32:53 +0530

    revision

--- src/main/java/sorting/SortingMain.java
+++ src/main/java/sorting/SortingMain.java
@@ -29,14 +29,55 @@ public class SortingMain {
             System.out.print(smallestSubArr(arr,k));
             System.out.println();
         }*/
-        int[] arr = new int[]{0,1,0,3,12};
-        moveZeroes(arr);
+        int[] arr = new int[]{12, 34, 45, 9, 8, 90, 3};
+        segregateEvenOdd(arr,arr.length);
         for (int t:arr
              ) {
             System.out.print(t+" ");
         }
 
     }
+
+
+    /**
+     * https://practice.geeksforgeeks.org/problems/segregate-even-and-odd-numbers4629/1
+     * Given an array Arr[], write a program that segregates even and odd numbers.
+     * The program should put all even numbers first in sorted order, and then odd numbers in sorted order.
+     *
+     * Example 1:
+     *
+     * Input:
+     * N = 7
+     * Arr[] = {12, 34, 45, 9, 8, 90, 3}
+     * Output: 8 12 34 90 3 9 45
+     * Explanation: Even numbers are 12, 34,
+     * 8 and 90. Rest are odd numbers. After
+     * sorting even numbers 8 12 34 90 and
+     * after sorting odd numbers 3 9 45. Then
+     * concat both.
+     * Example 2:
+     *
+     * Input:
+     * N = 5
+     * Arr[] = {0, 1, 2, 3, 4}
+     * Output: 0 2 4 1 3
+     * Explanation: 0 2 4 are even and 1 3
+     * are odd numbers.
+     * */
+     private static void segregateEvenOdd(int arr[], int n) {
+         int countOfEven = 0;
+         for (int i = 0; i < arr.length ; i++) {
+             if(arr[i]%2==0){
+                 if(i!=countOfEven){
+                     swap(arr,i,countOfEven);
+                 }
+                 countOfEven++;
+             }
+         }
+         //System.out.println("index of even: "+ countOfEven);
+         Arrays.sort(arr,0,countOfEven);
+         Arrays.sort(arr,countOfEven,arr.length);
+     }
 
 
     /**
