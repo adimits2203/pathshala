2973d0bfd1eff2751fee2ec38024abf31b512b4c
aditya <aditya.nigudkar@gmail.com>
2022-10-09 12:55:45 +0530

    Pascal triangle

--- src/main/java/hashing/HashingMain.java
+++ src/main/java/hashing/HashingMain.java
@@ -7,10 +7,62 @@ import java.util.stream.Collectors;
 public class HashingMain {
 
     public static void main(String[] args) {
-        HashingMain main = new HashingMain();
-        //System.out.println(main.palindromePairs(new String[]{"bb","bababab","baab","abaabaa","aaba","","bbaa","aba","baa","b"}));
-        //System.out.println(subarraySum(new int[]{[0,0,0,0,0,0,0,0,0,0]}, 3));
+        System.out.println(findMaxLength(new int[]{0,1,1,1,1,0,0,0}));
     }
+
+    /**
+     * https://leetcode.com/problems/contiguous-array/
+     * Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: nums = [0,1]
+     * Output: 2
+     * Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+     * Example 2:
+     *
+     * Input: nums = [0,1,0]
+     * Output: 2
+     * Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+     *
+     *
+     * Idea is to
+     * Use prefix sum approach,
+     * 1. convert all 0 to -1
+     * 2. calculate prefix sum and store it map
+     * 3. find the longest length where the prefix sum is zero(as 1 -1 = 0 if the count is same for 1 and -1(0))
+     * */
+     public static int findMaxLength(int[] nums) {
+         int ans = 0;
+         int n = nums.length;
+         Map<Integer,Integer> psumMap = new HashMap<>(); // store psum , first idx of occurrence
+         psumMap.put(0,-1);
+         // convert all zeros to -1
+         for (int i = 0; i < n; i++) {
+             if(nums[i]==0){
+                 nums[i] = -1;
+             }
+         }
+
+         // calculate psum
+         int psum = 0;
+         for (int i = 0; i < n; i++) {
+             psum+=nums[i];
+             // find out if it is repeating
+             if(psumMap.get(psum)!=null){
+                 // get the diff between first and this indx for this psum
+                 ans = Math.max(ans, i - psumMap.get(psum));
+             }
+             else{
+                 psumMap.put(psum,i);
+             }
+         }
+
+         return ans;
+
+     }
 
 
     /**
