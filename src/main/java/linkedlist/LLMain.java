2ce8b9a6bb3b39a5d6b49add04a1e47c340f7d7c
aditya <aditya.nigudkar@gmail.com>
2022-12-01 19:44:27 +0530

    Pascal triangle

--- src/main/java/linkedlist/LLMain.java
+++ src/main/java/linkedlist/LLMain.java
@@ -9,6 +9,103 @@ public class LLMain {
 
     }
 
+    /**
+     * https://leetcode.com/problems/copy-list-with-random-pointer/
+     *
+     * A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+     *
+     * Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+     *
+     * For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x and y in the copied list, x.random --> y.
+     *
+     * Return the head of the copied linked list.
+     *
+     * The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+     *
+     * val: an integer representing Node.val
+     * random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
+     * Your code will only be given the head of the original linked list.
+     *
+     * Input: head = [[1,1],[2,1]]
+     * Output: [[1,1],[2,1]]
+     *
+     * Idea is to create nodes connected to existing ll only and then after connecting the respective random nodes delink the old and new linked list
+     * */
+     public Node copyRandomList(Node head) {
+            Node curr = head;
+            while(curr!=null){
+                Node newNode = new Node(curr.val);
+                Node  temp = curr.next;
+                curr.next = newNode;
+                newNode.next = temp;
+                curr = newNode.next;
+            }
+
+            printLinkedList(head);
+
+            curr = head;
+
+            // add random pointers
+            while(curr!=null){
+                if(curr.random!=null){
+                    curr.next.random = curr.random.next;
+                }
+                else{
+                    curr.next.random = null;
+                }
+                curr = curr.next.next;
+            }
+
+            // delink lists
+            curr = head;
+            Node curr1 = head.next;
+            while(curr!=null && curr.next!=null){
+                curr.next = curr1.next;
+                if(curr.next!=null){
+                    curr = curr.next;
+                    curr1 = curr1.next;
+                }
+
+            }
+
+            printLinkedList(curr);
+            printLinkedList(curr1);
+
+            return curr1;
+
+
+     }
+
+    private void printLinkedList(Node head) {
+         Node curr = head;
+         while(curr!=null){
+             System.out.print(curr + " --> ");
+         }
+    }
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    ", random=" + random +
+                    '}';
+        }
+    }
+
+
+
     public static ListNode mergeKLists(ListNode[] lists) {
           if (lists.length==0) return null;
           return mergeKListsRec(lists,0, lists.length-1);
